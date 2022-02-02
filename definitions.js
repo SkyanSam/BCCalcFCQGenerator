b562a1a693288e080bfffc4014af368424128d0c
SkyanSam <samuelwiggins15@gmail.com>
2022-02-01 23:42:38 -0500

    mathtex hahahahahaah no tworking

---
+++ definitions.js
@@ -0,0 +1,12 @@
+//https://www.atomurl.net/math/
+//desmos can also be used
+
+
+function generate() {
+    document.getElementById("element1").innerHTML = "\\frac{d}{dx}\\sin x";
+    console.log(MathJax)
+    MathJax.typeset();
+}
+function reveal() {
+
+}
